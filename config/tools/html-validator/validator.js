ca4649d4f8fa56b4d8568e7bf9d6c069bd7bb7a6
白井渓祐 <shirai.keisuke@members.co.jp>
2018-11-13 19:06:12 +0900

    test

--- config/tools/html-validator/validator.js
+++ config/tools/html-validator/validator.js
@@ -1,6 +1,38 @@
 const fs = require("fs-extra");
+const globby = require("globby");
 const validate = require("html5-validator");
+let globOptions = {
+  matchBase: true,
+  onlyFiles: true
+};
+let errText;
 
-validate("src/index.html").then(result => {
-  console.log(result.messages);
+const writeValidate = path => {
+  validate(path).then(result => {
+    if (result.messages.length > 0) {
+      errText += "# " + path + "\n\n";
+      for (let err of result.messages)
+        errText +=
+          "* [" +
+          err.type +
+          "] " +
+          path +
+          ":" +
+          err.lastLine +
+          ":" +
+          err.firstColumn +
+          "  \n" +
+          err.message +
+          "\n```html\n" +
+          err.extract +
+          "\n```\n";
+      console.log(errText);
+    }
+  });
+};
+
+globby(["src/**/*.html"], globOptions).then(files => {
+  files.forEach(path => {
+    writeValidate(path);
+  });
 });

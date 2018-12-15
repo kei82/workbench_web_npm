ce7c47e60f935639a6dac586021f817efa829af9
kei82 <keikeikei10100802@gmail.com>
2018-12-15 15:41:59 +0900

    *

--- src/assets/babel/common.js
+++ src/assets/babel/common.js
@@ -1,10 +1,10 @@
 // DEMO
-import testA from './lib/test_a';
+import testA from "./lib/test_a";
 
 const testB = (text = "test B") => {
   console.log(text);
 };
-*_}}{;0-90-})(
+
 (async () => {
   await testA();
   testB();

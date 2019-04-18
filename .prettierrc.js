8336777c4e5ecbd45b051b1adc8332ada0f31bbb
kei82 <keikeikei10100802@gmail.com>
2019-04-18 23:48:50 +0900

    up

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 80,
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "none",
+  jsxBracketSameLine: false,
+  bracketSpacing: true,
+  arrowParens: "avoid",
+  proseWrap: "preserve",
+  endOfLine: "auto"
+};
